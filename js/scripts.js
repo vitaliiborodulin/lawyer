37c276d711a981b7e0feacc26639b3709150fef7
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-01-29 21:39:21 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
 $(function() {
     console.log(1)
+    const diplomaCarousel = new Carousel(document.querySelector(".carousel__diploma"), {
+    	'slidesPerPage' : 2,
+    	// 'friction' : 0.8,
+    	'infinite' : false, 
+    	// 'center': true, 
+    	// 'fill' : false,
+      // 'Autoplay': {
+      //   timeout: 1000,
+      // },
+    	'Dots': false,
+    	// Navigation: false,
+    });
+    
+    const sertCarousel = new Carousel(document.querySelector(".carousel__sert"), {
+    	'slidesPerPage' : 2,
+    	// 'friction' : 0.8,
+    	'infinite' : false, 
+    	// 'center': true, 
+    	// 'fill' : false,
+      // 'Autoplay': {
+      //   timeout: 1000,
+      // },
+    	'Dots': false,
+    	// Navigation: false,
+    });
+    // Fancybox.bind('[data-fancybox="gallery"]', {
+    //   Carousel: {
+    //     on: {
+    //       change: (that) => {
+    //         // Sync Carousel slide
+    //         mainCarousel.slideTo(mainCarousel.findPageForSlide(that.page), {
+    //           friction: 0,
+    //         });
+    //       },
+    //     },
+    //   },
+    // });
 });
