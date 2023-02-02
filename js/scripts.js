015b1ee0149221c721ef02365359aa5be0d357f0
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-02-02 10:37:21 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,10 @@ $(function() {
     	menu.removeClass('open');
     	burger.removeClass('header__burger--close');
     });
+    var cleave = new Cleave('input[type=tel]', {
+    	phone: true,
+    	phoneRegionCode: 'ru'
+    });
     const diplomaCarousel = new Carousel(document.querySelector(".carousel__diploma"), {
     	'slidesPerPage' : 2,
     	// 'friction' : 0.8,
@@ -49,6 +53,19 @@ $(function() {
       //   timeout: 1000,
       // },
     	'Dots': false,
+    	// Navigation: false,
+    });
+    
+    const feedbackCarousel = new Carousel(document.querySelector(".carousel__feedback"), {
+    	'slidesPerPage' : 1,
+    	// 'friction' : 0.8,
+    	'infinite' : false, 
+    	// 'center': true, 
+    	// 'fill' : false,
+      // 'Autoplay': {
+      //   timeout: 1000,
+      // },
+    	// 'Dots': false,
     	// Navigation: false,
     });
     // Fancybox.bind('[data-fancybox="gallery"]', {
